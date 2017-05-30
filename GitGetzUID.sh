255fe96f9bb014d43c78286220de6a53c40f917e
Rich Jackson <Richard.Jackson@walmart.com>
2017-05-30 16:06:46 -0500

    Adjust admin scripts

--- GitGetzUID.sh
+++ GitGetzUID.sh
@@ -1,12 +1,12 @@
-#/bin/sh
+#!/bin/sh
+# This script is used to sync MVS libs back to USS for Git compatibility
+# Update working directory for project
+# Update PDS names to local source lib names
 cd /apps/DEV/INFDS/zUID
-cp -T -U "//'SYS3.CICS.ZCLOUD.IP.ZUID.MD.V010000'" . 2> /dev/null
-cp -T -U "//'SYS3.CICS.ZCLOUD.IP.ZUID.TXT.V010000'" ./txt 2> /dev/null
-cp -T -U "//'SYS3.CICS.ZCLOUD.IP.ZUID.ASM.V010000'" ./asm 2> /dev/null
-cp -T -U "//'SYS3.CICS.ZCLOUD.IP.ZUID.CBL.V010000'" ./cbl 2> /dev/null
-cp -T -U "//'SYS3.CICS.ZCLOUD.IP.ZUID.CPY.V010000'" ./cpy 2> /dev/null
-cp -T -U "//'SYS3.CICS.ZCLOUD.IP.ZUID.EXEC.V010000'" ./exec 2> /dev/null
-cp -T -U "//'SYS3.CICS.ZCLOUD.IP.ZUID.IDCAMS.V010000'" ./idcams 2> /dev/null
-cp -T -U "//'SYS3.CICS.ZCLOUD.IP.ZUID.JCL.V010000'" ./jcl 2> /dev/null
-cp -T -U "//'SYS3.CICS.ZCLOUD.IP.ZUID.MAC.V010000'" ./mac 2> /dev/null
-cp -T -U "//'SYS3.CICS.ZCLOUD.IP.ZUID.RDO.V010000'" ./rdo 2> /dev/null
+cp -TUS a=.md "//'SYS3.CICS.ZCLOUD.IP.ZUID.MD.V010000'" . 2> /dev/null
+cp -TUS a=.txt "//'SYS3.CICS.ZCLOUD.IP.ZUID.TXT.V010000'" ./txt 2> /dev/null
+cp -TUS a=.asm "//'SYS3.CICS.ZCLOUD.IP.ZUID.ASM.V010000'" ./asm 2> /dev/null
+cp -TUS a=.cbl "//'SYS3.CICS.ZCLOUD.IP.ZUID.CBL.V010000'" ./cbl 2> /dev/null
+cp -TUS a=.exec "//'SYS3.CICS.ZCLOUD.IP.ZUID.EXEC.V010000'" ./exec 2> /dev/null
+cp -TUS a=.jcl "//'SYS3.CICS.ZCLOUD.IP.ZUID.JCL.V010000'" ./jcl 2> /dev/null
+cp -TUS a=.rdo "//'SYS3.CICS.ZCLOUD.IP.ZUID.RDO.V010000'" ./rdo 2> /dev/null
--- MakeDirs.sh
+++ MakeDirs.sh
@@ -1,11 +1,15 @@
-#/bin/sh
+#!/bin/sh
+# This script is only really needed if upstreaming from an original
+#      non-Git copy of the source libs
+#   It will predefine the necessary dirs in prep for copy from PDSE
+# Update working directory for project
 cd /apps/DEV/INFDS/zUID
-mkdir ./txt 2> /dev/null
 mkdir ./asm 2> /dev/null
 mkdir ./cbl 2> /dev/null
 mkdir ./cpy 2> /dev/null
 mkdir ./exec 2> /dev/null
-mkdir ./idcams 2> /dev/                                                 null
+mkdir ./idcams 2> /dev/null
 mkdir ./jcl 2> /dev/null
 mkdir ./mac 2> /dev/null
 mkdir ./rdo 2> /dev/null
+mkdir ./txt 2> /dev/null
